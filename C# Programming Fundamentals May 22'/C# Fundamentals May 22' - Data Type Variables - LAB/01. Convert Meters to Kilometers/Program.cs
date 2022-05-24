b6ab36bcea2d06450747f31b197ffeddf9d025e6
achkatam <100487082+achkatam@users.noreply.github.com>
2022-05-24 12:58:13 -0400

    Uploaded source code first problem

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Data Type Variables - LAB/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will be given an integer that will be a distance in meters.Create a program that converts meters to kilometers formatted to the second decimal point.
+
+            //input in meters
+            int meters = int.Parse(Console.ReadLine());
+
+            double km = meters * 0.001;
+
+            Console.WriteLine($"{(km):f2}");
+
+
+        }
+    }
+}
