95359ddbf534572d5e5e2b97917aaa2679e89dde
achkatam <100487082+achkatam@users.noreply.github.com>
2022-05-28 13:09:01 -0400

    Added two more problem solutions

---
+++ C# Programming Fundamentals May 22'/C# - Fundamentals - May '22 -Data Types And Variables More Exercise/FromLeftToRight/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace FromLeftToRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive a number that represents how many lines we will get as input.On the next N lines, you will receive a string with 2 numbers separated by a single space. You need to compare them.If the left number is greater than the right number, you need to print the sum of all digits in the left number, otherwise, print the sum of all digits in the right number.
+
+            //input
+            int n = int.Parse(Console.ReadLine());
+
+            //Attempt solution
+            //add-ons
+            long sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] num = input.Split(' ');
+                long firstNum = long.Parse(num[0]);
+                long secondNum = long.Parse(num[1]);
+
+                if(firstNum > secondNum)
+                {
+                    while (firstNum !=0)
+                    {
+                        sum += firstNum % 10;
+                        firstNum /= 10;
+                    }
+                    Console.WriteLine($"{Math.Abs(sum)}");
+                    sum = 0;
+                }
+                else if (firstNum <= secondNum)
+                {
+                    while (secondNum !=0)
+                    {
+                        sum += secondNum % 10;
+                        secondNum /= 10;
+                    }
+                    Console.WriteLine($"{Math.Abs(sum)}");
+                    sum = 0;
+                }
+            }
+        }
+    }
+}
---
+++ C# Programming Fundamentals May 22'/C# - Fundamentals - May '22 -Data Types And Variables More Exercise/Prime Check Recfactor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Prime_Check_Recfactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You are given a program that checks if numbers in a given range[2...N] are prime. For each number is printed "{number} -> {true or false}".The code, however, is not very well written.Your job is to modify it in a way that is easy to read and understand.
+            //Code
+            //Sample Code
+            //int ___Do___ = int.Parse(Console.ReadLine());
+            //            for (int takoa = 2; takoa <= ___Do___; takoa++)
+            //            {
+            //                bool takovalie = true;
+            //                for (int cepitel = 2; cepitel < takoa; cepitel++)
+            //                {
+            //                    if (takoa % cepitel == 0)
+            //                    {
+            //                        takovalie = false;
+            //                        break;
+            //                    }
+            //                }
+            //Console.WriteLine("{0} -> {1}", takoa, takovalie);
+
+            //input
+            int n = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+
+            for (int i = 2; i <= n; i++)
+            {
+                isPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        
+                    }
+                }
+                Console.WriteLine($"{i} -> {isPrime.ToString().ToLower()}");
+            }
+
+        }
+    }
+}
