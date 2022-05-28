509ee01a2a90bf4adcedc7117f485f7ef831b1a2
achkatam <100487082+achkatam@users.noreply.github.com>
2022-05-28 13:08:33 -0400

    Added 2 more problem solutions

---
+++ C# Programming Fundamentals May 22'/C# - Fundamentals - May '22 -Data Types And Variables More Exercise/03. Floating Equality/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that safely compares floating - point numbers(double) with precision eps = 0.000001.Note that we cannot directly compare two floating - point numbers a and b by a== b because of the nature of the floating - point arithmetic.Therefore, we assume two numbers are equal if they are more close to each other than some fixed constant eps. 
+            //You will receive two lines, each containing a floating-point number.Your task is to compare the values of the two numbers.
+
+            //input
+            const double precision = 0.000001;
+            
+            double numA = double.Parse(Console.ReadLine());
+            double numB = double.Parse(Console.ReadLine());
+
+            //add-ons
+            double result = Math.Abs(numA - numB);
+            bool isValid = precision > result;
+            //Attempt solution
+
+            string result1 = precision > result ? "True" : "False";
+            Console.WriteLine(result1);
+
+        }
+    }
+}
---
+++ C# Programming Fundamentals May 22'/C# - Fundamentals - May '22 -Data Types And Variables More Exercise/Decrypting Message/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive a key(integer) and n characters afterward. Add the key to each of the characters and append them to a message. At the end print the message, which you decrypted. 
+            //Input
+            //•	On the first line, you will receive the key
+            //•	On the second line, you will receive n – the number of lines, which will follow
+            //•	On the next n lines – you will receive lower and uppercase characters from the Latin alphabet
+
+            //input
+            int key = int.Parse(Console.ReadLine());//this integer will be added to a letter
+            int n = int.Parse(Console.ReadLine());
+            //add-ons
+            string message = string.Empty;
+            //Attempt Solution
+            for (int i = 0; i < n; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                int result = key + (int)input;
+                char endChar = (char)result;
+                message += endChar;
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
