97683c2e7e7bb37747193fe5717a4548de21ec89
achkatam <100487082+achkatam@users.noreply.github.com>
2022-05-28 13:07:56 -0400

    Added solution

---
+++ C# Programming Fundamentals May 22'/C# - Fundamentals - May '22 -Data Types And Variables More Exercise/01. Data Type Finder/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _01._Data_Type_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive input until you receive "END".Find what data type is the input. Possible data types are:
+            //•	Integer
+            //•	Floating point 
+            //•	Characters
+            //•	Boolean
+            //•	Strings
+            //Print the result in the following format: "{input} is {data type} type"
+
+            //input
+            string input = Console.ReadLine();
+            //add-ons
+            int valueInt = 0;
+            float valueFloat = 0.0f;
+            char valueChar;
+            bool valueBool;
+
+            //Attempt solution
+            while (input != "END")
+            {
+                if(int.TryParse(input, out valueInt))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (float.TryParse(input, out valueFloat))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+
+                }
+                else if (char.TryParse(input, out valueChar))
+                {
+                    Console.WriteLine($"{input} is character type");
+
+                }
+                else if (bool.TryParse(input, out valueBool))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
