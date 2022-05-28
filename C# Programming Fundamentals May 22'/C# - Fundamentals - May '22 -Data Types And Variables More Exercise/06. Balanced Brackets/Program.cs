8f7683dd55ef42fcea6f1f959df1abcfbc0f0bdb
achkatam <100487082+achkatam@users.noreply.github.com>
2022-05-28 13:20:51 -0400

    Added the last solution from More Exercises

---
+++ C# Programming Fundamentals May 22'/C# - Fundamentals - May '22 -Data Types And Variables More Exercise/06. Balanced Brackets/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive n lines.On those lines, you will receive one of the following:
+            //•	Opening bracket – “(“,
+            //•	Closing bracket – “)” or
+            //•	Random string
+            //Your task is to find out if the brackets are balanced.That means after every closing bracket should follow an opening one.Nested parentheses are not valid, and if two consecutive opening brackets exist, the expression should be marked as unbalanced.
+
+            //input
+            int n = int.Parse(Console.ReadLine());
+            //add-ons
+            bool isOpeninBracketFirst = false, isUnabalanced = false;
+            //Solution first
+            List<string> openingBrackets = new List<string>();
+            List<string> closingBrackets = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string input = Console.ReadLine();
+                if (input == "(")
+                {
+                    isOpeninBracketFirst = true;
+                    openingBrackets.Add(input);
+                }
+                else if (input == ")")
+                {
+                    if (!isOpeninBracketFirst) isUnabalanced = true;
+
+                    isOpeninBracketFirst = false;
+                    closingBrackets.Add(input);
+                }
+            }
+            if (openingBrackets.Count == closingBrackets.Count && !isUnabalanced)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+
+        }
+    }
+}
