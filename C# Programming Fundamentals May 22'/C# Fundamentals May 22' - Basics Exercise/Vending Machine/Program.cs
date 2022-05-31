b05a3f19d22a4ff542fc7958dcb534d24f6e3b43
achkatam <100487082+achkatam@users.noreply.github.com>
2022-05-31 14:48:54 -0400

    Added solution to the problem using Arrays[]

--- C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Basics Exercise/Vending Machine/Program.cs
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Basics Exercise/Vending Machine/Program.cs
@@ -20,66 +20,119 @@ namespace Vending_Machine
             //When a customer has enough money to buy the selected product, print "Purchased {product name}", otherwise print "Sorry, not enough money", and continue to the next line.
             //When the "End" command is given print the reminding balance, formatted to the second decimal point: "Change: {money left}".
 
+            //First Solution - most common
             //input
-            string command = Console.ReadLine();
+            //string command = Console.ReadLine();
 
-            double totalMoneyAccumulated = 0;
-            while (command != "Start")
+            //double totalMoneyAccumulated = 0;
+            //while (command != "Start")
+            //{
+            //    double coin = double.Parse(command);
+            //    if (coin == 0.1 || coin == 0.2 || coin == 0.5 || coin == 1 || coin == 2)
+            //    {
+            //        totalMoneyAccumulated += coin;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Cannot accept {coin}");
+            //    }
+            //    command = Console.ReadLine();
+            //}
+            //command = Console.ReadLine();
+            //double totalPrice = 0;
+
+            //while (command != "End")
+            //{
+            //    switch (command)
+            //    {
+            //        case "Nuts":
+            //            totalPrice = 2;
+            //            break;
+            //        case "Water":
+            //            totalPrice = 0.7;
+            //            break;
+            //        case "Crisps":
+            //            totalPrice = 1.5;
+            //            break;
+            //        case "Soda":
+            //            totalPrice = 0.8;
+            //            break;
+            //        case "Coke":
+            //            totalPrice = 1;
+            //            break;
+            //        default:
+            //            Console.WriteLine($"Invalid product");
+            //            command = Console.ReadLine();
+            //            continue;
+            //    }
+
+            //    if (totalMoneyAccumulated >= totalPrice)
+            //    {
+            //        totalMoneyAccumulated -= totalPrice;
+            //        Console.WriteLine($"Purchased {command.ToLower()}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Sorry, not enough money");
+            //    }
+
+            //    command = Console.ReadLine();
+            //}
+
+            //Console.WriteLine($"Change: {totalMoneyAccumulated:f2}");
+
+            //Second Solution using arrays[]
+            //Creating arrays
+            string[] products = new string[5] { "Nuts", "Water", "Crisps", "Soda", "Coke" };
+            double[] prices = new double[5] { 2, 0.7, 1.5, 0.8, 1 };
+
+            double[] coins = new double[] { 0.1, 0.2, 0.5, 1, 2 };
+
+            double coin = 0;
+            double balance = 0;
+            //input
+            string input = "";
+            while ((input = Console.ReadLine()) != "Start")
             {
-                double coin = double.Parse(command);
-                if (coin == 0.1 || coin == 0.2 || coin == 0.5 || coin == 1 || coin == 2)
+                coin = double.Parse(input);
+
+                if (coins.Contains(coin))
                 {
-                    totalMoneyAccumulated += coin;
+                    balance += coin;
                 }
                 else
                 {
                     Console.WriteLine($"Cannot accept {coin}");
                 }
-                command = Console.ReadLine();
             }
-            command = Console.ReadLine();
-            double totalPrice = 0;
 
-            while (command != "End")
+            while ((input = Console.ReadLine()) != "End")
             {
-                switch (command)
+                if (!products.Contains(input))
                 {
-                    case "Nuts":
-                        totalPrice = 2;
-                        break;
-                    case "Water":
-                        totalPrice = 0.7;
-                        break;
-                    case "Crisps":
-                        totalPrice = 1.5;
-                        break;
-                    case "Soda":
-                        totalPrice = 0.8;
-                        break;
-                    case "Coke":
-                        totalPrice = 1;
-                        break;
-                    default:
-                        Console.WriteLine($"Invalid product");
-                        command = Console.ReadLine();
-                        continue;
+                    Console.WriteLine("Invalid product");
+                    continue;
                 }
 
-                if (totalMoneyAccumulated >= totalPrice)
+                for (int productNum = 0; productNum < 5; productNum++)
                 {
-                    totalMoneyAccumulated -= totalPrice;
-                    Console.WriteLine($"Purchased {command.ToLower()}");
-                }
-                else
-                {
-                    Console.WriteLine($"Sorry, not enough money");
+                    if (input == products[productNum])
+                    {
+                        if (balance - prices[productNum] < 0)
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Purchased {input.ToLower()}");
+                            balance -= prices[productNum];
+                        }
+                        break;
+                    }
                 }
 
-                command = Console.ReadLine();
             }
-
-            Console.WriteLine($"Change: {totalMoneyAccumulated:f2}");
-
+            Console.WriteLine($"Change: {balance:f2}");
         }
     }
 }
