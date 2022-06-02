10643fa0e19ccd797e53b61d2ceee53d130a7482
achkatam <100487082+achkatam@users.noreply.github.com>
2022-06-02 09:17:42 -0400

    Created new sub-repo for Arrays from the lab. Added also solutions for two of the problems

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Arrays LAB/Condense Array to Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] condensed = new int[numbers.Length - 1];
+
+            //Solution
+            while (numbers.Length > 1)
+            {
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    condensed[i] = numbers[i] + numbers[i + 1];
+
+                    if (i == numbers.Length - 2)
+                    {
+                        numbers = condensed;
+                        condensed = new int[numbers.Length - 1];
+                    }
+                }
+            }
+            Console.Write(numbers[0]);
+        }
+    }
+}
---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Arrays LAB/DayOfWeek/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int dayfOfWeek = int.Parse(Console.ReadLine());
+            string[] days =
+            {
+                "Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday", "Sunday"
+            };
+            //Solution
+            //if (dayfOfWeek >= 1 && dayfOfWeek <= 7)
+            //{
+            //    Console.WriteLine(days[dayfOfWeek - 1]);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Day!");
+            //}
+
+            //Ternary operator
+            string result = (dayfOfWeek >= 1 && dayfOfWeek <= 7) ?
+                days[dayfOfWeek - 1] : "Invalid day!";
+            Console.WriteLine(result);
+        }
+    }
+}
