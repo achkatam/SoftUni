813a26bbf6a96d2ad6c241579dd14533debec034
achkatam <100487082+achkatam@users.noreply.github.com>
2022-06-03 22:20:49 -0400

    Added one more solution.

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Arrays Exercise/8. Magic Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int givenNum = int.Parse(Console.ReadLine());
+
+            //Solution
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                //int number = num[i];
+                for (int j = i + 1; j < num.Length; j++)
+                {
+                    if (num[i] + num[j] == givenNum)
+                    {
+                        Console.WriteLine($"{num[i]} {num[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
