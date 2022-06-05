c10ab177a922aed9300d3aee261522ee84c495d6
achkatam <100487082+achkatam@users.noreply.github.com>
2022-06-05 14:46:40 -0400

    Added one more solution to Common Elements/Program.cs

--- C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Arrays Exercise/Common Elements/Program.cs
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Arrays Exercise/Common Elements/Program.cs
@@ -26,6 +26,22 @@ namespace Common_Elements
                 }
             }
 
+            //Second solution
+            //Create array 
+            string[] firstArr = Console.ReadLine().Split();
+            string[] secondArr = Console.ReadLine().Split();
+            //For loop
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                for (int j = 0; j < secondArr.Length; j++)
+                {
+                    if (firstArr[i] == secondArr[j])
+                    {
+                        Console.Write($"{firstArr[i]} ");
+                    }
+                }
+            }
+
 
         }
     }
