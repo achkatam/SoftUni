9321e38aa89a2f2a003ea485aa935e1cdfd88c8d
achkatam <100487082+achkatam@users.noreply.github.com>
2022-06-05 13:34:07 -0400

    Added two more problem solutions. Fibonacci problem has two different solution - one is using recursive method, the second one is using classes

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Arrays More Exercises/Pascal Triangle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input the heght of the triangle
+            int height = int.Parse(Console.ReadLine());
+
+            // Create an jagged array - array of arrays
+            int[][] array = new int[height][];
+
+            // For loop
+            for (int row = 0; row < height; row++)
+            {
+                array[row] = new int[row + 1];
+
+                for (int col = 0; col <= row; col++)
+                {
+                    if (col == 0 || col == row)
+                    {
+                        //The triangle starts from 1 to...
+                        array[row][col] = 1;
+                        Console.Write($"{array[row][col]} ");
+                    }
+                    else
+                    {
+                        array[row][col] = array[row - 1][col - 1] + array[row - 1][col];
+                        Console.Write($"{array[row][col]} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Arrays More Exercises/Recursive Fibonacci/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input for the Fibonacci number
+            int inputNum = int.Parse(Console.ReadLine());
+
+            ////Solution using class
+            //Console.WriteLine(GetFibonacciNum(inputNum));
+
+            //Create an array
+            int[] numbers = new int[inputNum];
+
+            //Solution using recursive method
+
+            //Creating For loop which follow the all the numbers in the array numbers[]
+            for (int nums = 0; nums < numbers.Length; nums++)
+            {
+                if (nums == 0 || nums == 1)
+                {
+                    numbers[nums] = 1;
+                }
+                else
+                {
+                    numbers[nums] = numbers[nums - 1] + numbers[nums - 2];
+                }
+                
+            }
+            Console.WriteLine(numbers[numbers.Length - 1]);
+        }
+        //static int GetFibonacciNum(int inputNum)
+        //{
+        //    //Created a new class which contains the formula 
+        //    if (inputNum == 1 || inputNum == 2)
+        //    {
+        //        return 1;
+        //    }
+        //    return GetFibonacciNum(inputNum - 1) + GetFibonacciNum(inputNum - 2);
+        //}
+    }
+}
