6f30ec5322a1618f0fc9a3e114c63c7acbb871e7
achkatam <100487082+achkatam@users.noreply.github.com>
2022-06-12 13:55:26 -0400

    Added solution to Tribonacci Sequence

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Methods More Exercises/Tribonacci Sequence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tribonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            //Output
+            Console.WriteLine(string.Join(" ", TribonacciSequence(num)));
+        }
+        static int[] TribonacciSequence(int number)
+        {
+            //Create array so we can check the elements 1 by 1, using For loop
+            int[] tribonacciArray = new int[number];
+
+            for (int num = 0; num < tribonacciArray.Length; num++)
+            {
+                if (num == 0 || num == 1)
+                {
+                    tribonacciArray[num] = 1;
+                }
+                else if (num == 2)
+                {
+                    tribonacciArray[num] = 2;
+                }
+                else
+                {
+                    tribonacciArray[num] = tribonacciArray[num - 3] +
+                        tribonacciArray[num - 2] + tribonacciArray[num - 1];
+                }
+            }
+            return tribonacciArray;
+        }
+    }
+}
