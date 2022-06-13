a9be788d46abd6a35249649d2dde4e26beb175f9
achkatam <100487082+achkatam@users.noreply.github.com>
2022-06-13 10:41:07 -0400

    Updated solution for Top Number

--- C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Methods Exercise/Top Number/Program.cs
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Methods Exercise/Top Number/Program.cs
@@ -1,47 +1,43 @@
 ﻿using System;
 
-namespace Top_Number
+namespace Top_Integer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Print all the top numbers
-            //Top number is: divisible by 8 => 8, 17, 88
-            //Hold at least one odd digit = 232, 707, 87578
-            //Examples: 17, 161, 251, 4310, 123200
+            // A top number is an integer that holds the following properties:
+            //•	Its sum of digits is divisible by 8, e.g. 8, 17, 88
+            //•	Holds at least one odd digit, e.g. 232, 707, 87578
+            //•	Some examples of top numbers are: 17, 161, 251, 4310, 123200
+            //You will receive a single integer from the console, representing the end value. Examples
 
             //input
-            int number = int.Parse(Console.ReadLine());
+            int integer = int.Parse(Console.ReadLine());
 
-            //Output the method
-            TopNumber(number);
+            //Output
+            TopNumber(integer);
         }
-        static void TopNumber(int number)
+
+        static void TopNumber(int num)
         {
-            for (int i = 1; i <= number; i++)
+            //Start with For loop, create boolean isOdd,int sum and int currNum
+            for (int i = 1; i <= num; i++)
             {
-                //Create boolean isOdd, currNum and check the constraints, then print all the nums
+                //boolean
                 bool isOdd = false;
-                int currNum = i;
-                //The sum represents the number BY SUM OF DIGITS
+                //sum
                 int sum = 0;
-                //Check the digits of the number 1 by 1
+
+                int currNum = i;
                 while (currNum != 0)
                 {
-                    sum += currNum % 10;//Takes the last digit of the number
-                    //Check the last digit if it's odd
-                    if ((currNum % 10) % 2 != 0)
-                    {
-                        isOdd = true;
-                    }
-                    currNum /= 10;
+                    sum += currNum % 10;//this variable sums up the digits in num
+                    if ((currNum % 10) % 2 != 0) isOdd = true; // Check if the last digit taken is odd or even
+                    currNum /= 10;//removes the last digit of the num
                 }
-                //Check if the SUM of DIGITS is divisible by 8 and isOdd
-                if (isOdd && sum % 8 == 0)
-                {
-                    Console.WriteLine(i);
-                }
+                //Check if the sum is divisible by and isOdd = true
+                if (sum % 8 == 0 && isOdd) Console.WriteLine(i);
             }
         }
     }
