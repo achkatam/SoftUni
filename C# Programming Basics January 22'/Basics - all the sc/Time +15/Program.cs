e857f06473400e9e5fd7f19aa8174c352e971d0d
achkatam <100487082+achkatam@users.noreply.github.com>
2022-06-16 09:26:16 -0400

    Added solution for Time +15 Basic course

---
+++ C# Programming Basics January 22'/Basics - all the sc/Time +15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Time__15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            TimeSpan hour = TimeSpan.FromHours(int.Parse(Console.ReadLine()));
+            TimeSpan min = TimeSpan.FromMinutes(int.Parse(Console.ReadLine()));
+            TimeSpan time = hour + min.Add(TimeSpan.FromMinutes(15));
+            //output
+            Console.WriteLine($"{time.Hours}:{time.Minutes:00}");
+        }
+    }
+}
