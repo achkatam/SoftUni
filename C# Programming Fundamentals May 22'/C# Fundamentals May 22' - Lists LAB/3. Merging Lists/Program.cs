839521d1647fbf8ea937f6a750b28c39868eec2e
achkatam <100487082+achkatam@users.noreply.github.com>
2022-06-18 12:00:30 -0400

    Updated solution

--- C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Lists LAB/3. Merging Lists/Program.cs
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Lists LAB/3. Merging Lists/Program.cs
@@ -2,33 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _3._Merging_Lists
+namespace Merging_Lists
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Create three lists, merge the first two  and print them
-            List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
-            List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //You are going to receive two lists of numbers.Create a list that contains the numbers from both of the lists. The first element should be from the first list, the second from the second list, and so on.If the length of the two lists is not equal, just add the remaining elements at the end of the list.
 
-            List<int> result = new List<int>();
-            //Which one has larger one
-            int biggerCount = Math.Max(firstList.Count, secondList.Count);
+            //            Hints:
+            //•	Read the two lists
+            //•	Create a result list
+            //•	Start looping through them until you reach the end of the smallest one
+            //•	Finally, add the remaining elements(if there are any) to the end of the list
 
-            for (int i = 0; i < biggerCount; i++)
+            //Create 2 list
+            var firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //Create result list
+            var result = new List<int>();
+
+            //Find the larger one list among the first 2
+            int largerList = Math.Max(firstList.Count, secondList.Count);
+            //For loop to iterate thru the lists
+            for (int i = 0; i < largerList; i++)
             {
-                if (firstList.Count > i)
-                {
-                    result.Add(firstList[i]);
-                }
-                if (secondList.Count > i)
-                {
-                    result.Add(secondList[i]);
-                }
+                if (firstList.Count > 1) result.Add(firstList[i]);
+                if (secondList.Count > i) result.Add(secondList[i]);
+
             }
-            //Output
-            Console.WriteLine(string.Join(" ", result));
+                Console.WriteLine(string.Join(' ', result));
 
         }
     }
