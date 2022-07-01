ae780632942490eeaabf69776974723c9a49d8cf
achkatam <100487082+achkatam@users.noreply.github.com>
2022-06-30 22:03:58 -0400

    Added solution from Objects And Classes

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Objects And Classes LAB/02. Big Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive a number N in the range [0 - 1000]. Calculate the Factorial of N and print out the result.
+            int number = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
