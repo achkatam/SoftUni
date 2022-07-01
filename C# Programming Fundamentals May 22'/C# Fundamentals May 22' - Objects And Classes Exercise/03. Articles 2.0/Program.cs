0eddb170f927de780b2aa08ce51a5f4fb5110b7b
achkatam <100487082+achkatam@users.noreply.github.com>
2022-07-01 12:48:45 -0400

    Two more solutions added from Exercise - Objects and Classes

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Objects And Classes Exercise/03. Articles 2.0/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Articles_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfArticles = int.Parse(Console.ReadLine());
+
+            List<Article> articles = new List<Article>();
+
+            for (int i = 0; i < countOfArticles; i++)
+            {
+                string[] currArticle = Console.ReadLine().Split(", ");
+                var article = new Article(currArticle[0], currArticle[1], currArticle[2]);
+
+                articles.Add(article);
+            }
+
+            string line = Console.ReadLine();
+
+            foreach (var article in articles)
+            {
+                Console.WriteLine(article);
+            }
+
+        }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public Article()
+        {
+
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString() => $"{Title} - { Content}: { Author}";
+
+    }
+}
---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Objects And Classes Exercise/04. Students/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	On the first line, you will receive a number n - the count of all students
+            //•	On the next n lines, you will be receiving information about these students in the following format: "{first name} {second name} {grade}"
+
+            int countOfStudents = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < countOfStudents; i++)
+            {
+                string[] currStudent = Console.ReadLine().Split();
+                var student = new Student(currStudent[0], currStudent[1], double.Parse(currStudent[2]));
+
+                students.Add(student);
+            }
+            //Sort by grade
+            List<Student> sortedStudents = students.OrderByDescending(s => s.Grade).ToList();
+
+            sortedStudents.ForEach(student => Console.WriteLine(student));
+
+            //foreach (var student in sortedStudents)
+            //{
+            //    Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+            //}
+
+        }
+    }
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+        public override string ToString() => $"{FirstName} {LastName}: {Grade:f2}";
+        
+    }
+}
