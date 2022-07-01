47d9d00207c946b99ad9d31018e72fb00adf7cfe
achkatam <100487082+achkatam@users.noreply.github.com>
2022-07-01 12:47:57 -0400

    Added solutions from Objects and Classes - Exercise

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Objects And Classes Exercise/02. Articles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] currentArticle = Console.ReadLine().Split(", ");
+            var article = new Article(currentArticle[0], currentArticle[1], currentArticle[2]);
+
+            int numOfChanges = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfChanges; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(": ");
+                string cmd = tokens[0];
+                string argument = tokens[1];
+
+                switch (cmd)
+                {
+                    case "Edit":
+                        article.Edit(argument);
+                        break;
+                    case "ChangeAuthor":
+                        article.ChangeAuthor(argument);
+                        break;
+                    case "Rename":
+                        article.Rename(argument);
+                        break;
+                }
+            }
+            Console.WriteLine(article);
+        }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public Article()
+        {
+
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Rename(string title) => Title = title;
+        public void Edit(string content) => Content = content;
+        public void ChangeAuthor(string author) => Author = author;
+        public override string ToString() =>  $"{Title} - { Content}: { Author}";
+        
+    }
+}
