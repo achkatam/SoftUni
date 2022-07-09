cd55ca91ac3309302f9938b9caa92571403f32de
achkatam <100487082+achkatam@users.noreply.github.com>
2022-07-09 12:38:27 -0400

    Made some changes in the solution for problem Student Academy

--- C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Associative Arrays Exercise/06. Student Academy/Program.cs
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Associative Arrays Exercise/06. Student Academy/Program.cs
@@ -8,7 +8,6 @@ namespace _06._Student_Academy
     {
         static void Main(string[] args)
         {
-
             //Create a program that keeps the information about students and their grades. 
             // You will receive n pair of rows.First, you will receive the student's name, after that, you will receive their grade. Check if the student already exists and if not, add him. Keep track of all grades for each student.
             //When you finish reading the data, keep the students with an average grade higher than or equal to 4.50.
@@ -16,33 +15,38 @@ namespace _06._Student_Academy
             //"{name} –> {averageGrade}"
             //Format the average grade to the 2nd decimal place.
 
-
+            //Create dictionary of students and list of their grades
             var students = new Dictionary<string, List<double>>();
+            // const avgGrade
+            const double AVG_GRADE = 4.5;
+            //variables for the count of all students
+            int coundOfStudents = int.Parse(Console.ReadLine());
 
-            int n = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < n; i++)
+            //for loop to iterate thru each student
+            for (int i = 0; i < coundOfStudents; i++)
             {
-                string studentName = Console.ReadLine();
+                string name = Console.ReadLine();
                 double grade = double.Parse(Console.ReadLine());
 
-                if (!students.ContainsKey(studentName))
+                if (!students.ContainsKey(name))
                 {
-                    students[studentName] = new List<double>();
+                    students[name] = new List<double>();
                 }
-                students[studentName].Add(grade);
-
-
+                //Add the student and his/her grade to the list of grades
+                students[name].Add(grade);
             }
 
-            foreach (var student in students)
+            PrintResult(students);
+
+        }
+
+        private static void PrintResult(Dictionary<string, List<double>> students)
+        {
+            const double AVG_GRADE = 4.5;
+
+            foreach (var student in students.Where(s => s.Value.Average() >= AVG_GRADE))
             {
-                string studentName = student.Key;
-                double avgGrade = student.Value.Average();
-                if (avgGrade >= 4.5)
-                {
-                    Console.WriteLine($"{student.Key} -> {avgGrade:f2}");
-                }
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
             }
         }
     }
