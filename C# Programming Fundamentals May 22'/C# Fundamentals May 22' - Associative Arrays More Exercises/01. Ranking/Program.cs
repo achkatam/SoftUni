cfa36eac15c81d0bc1cea93efbae7320124ea012
achkatam <100487082+achkatam@users.noreply.github.com>
2022-07-10 11:48:18 -0400

    Added two solutions from more exercises

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Associative Arrays More Exercises/01. Ranking/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Here comes the final and the most interesting part – the Final ranking of the candidate-interns. The final ranking is determined by the points of the interview tasks and from the exams in SoftUni. Here is your final task. You will receive some lines of input in the format "{contest}:{password for contest}" until you receive "end of contests". Save that data because you will need it later. After that, you will receive another type of inputs in the format "{contest}=>{password}=>{username}=>{points}" until you receive "end of submissions". Here is what you need to do. 
+
+
+            //In the end, you have to print the info for the user with the most points in the format "Best candidate is {user} with total {total points} points.".After that print all students ordered by their names.For each user print each contest with the points in descending order.See the examples
+
+            //Create two dictionaries for contestInfo and student submission, which will be dictionary in dictionary
+            //  contestName and password = string, string
+            var contests = new Dictionary<string, string>();
+            // studentName and points
+            var studentsInfo = new Dictionary<string, Dictionary<string, int>>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(":"
+                    , StringSplitOptions.RemoveEmptyEntries);
+                if (command[0] == "end of contests") break;
+                //"{contest}:{password for contest}"
+                string contest = command[0];
+                string contestPassword = command[1];
+
+                contests[contest] = contestPassword;
+            }
+
+            //"{contest}=>{password}=>{username}=>{points}" until you receive "end of submissions"
+            while (true)
+            {
+                string[] cmds = Console.ReadLine().Split("=>",
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (cmds[0] == "end of submissions") break;
+
+                string contestName = cmds[0];
+                string password = cmds[1];
+                string username = cmds[2];
+                int points = int.Parse(cmds[3]);
+
+                //Nested if statements
+                //•	Check if the contest is valid(if you received it in the first type of input)
+                if (contests.ContainsKey(contestName))
+                {
+                    //•	Check if the password is correct for the given contest
+                    if (contests.ContainsValue(password))
+                    {
+                        //•	Save the user with the contest they take part in (a user can take part in many contests) and the points the user has in the given contest.If you receive the same contest and the same user updates the points only if the new ones are more than the older ones.
+                        if (studentsInfo.ContainsKey(username))
+                        {
+                            if (studentsInfo[username].ContainsKey(contestName))
+                            {
+                                if (studentsInfo[username][contestName] < points)
+                                {
+                                    studentsInfo[username][contestName] = points;
+                                }
+                            }
+                            else
+                            {
+                                studentsInfo[username].Add(contestName, points);
+                            }
+                        }
+                        else
+                        {
+                            studentsInfo[username] = new Dictionary<string, int> { { contestName, points } };
+                        }
+                    }
+                }
+            }
+            string bestStudent = " ";
+            int bestScore = 0;
+
+            foreach (var student in studentsInfo)
+            {
+                if (studentsInfo[student.Key].Values.Sum() > bestScore)
+                {
+                    bestStudent = student.Key;
+                    bestScore = studentsInfo[student.Key].Values.Sum();
+                }
+            }
+
+            Console.WriteLine($"Best candidate is {bestStudent} with total {bestScore} points.");
+            Console.WriteLine("Ranking: ");
+
+            foreach (var user in studentsInfo.OrderBy(n=>n.Key))
+            {
+                //.". After that print all students ordered by their names. For each user print each contest with the points in descending order. 
+                Console.WriteLine(user.Key);
+                foreach (var contest in user.Value.OrderByDescending(p => p.Value))
+                {
+                    Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Associative Arrays More Exercises/02. Judge/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Judge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //You know the judge system, right?! Your job is to create a program similar to the Judge system. 
+            //You will receive several input lines in one of the following formats:
+            //"{username} -> {contest} -> {points}"
+            //The constestName and username are strings, the given points will be an integer number.You need to keep track of every contest and individual statistics of every user. 
+            //
+
+            //Create two dictionaries - one for contest and one for points
+            var contests = new Dictionary<string, Dictionary<string, int>>();
+
+            var totalPoints = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(" -> "
+                    , StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "no more time") break;
+                //      username - contest, points
+                //Create variables for the fore aforementioned inputs
+                string username = command[0];
+                string contestName = command[1];
+                int points = int.Parse(command[2]);
+
+                //You should check if such a contest already exists, and if not, add it, otherwise, check if the current user Is participating in the contest, if he is participating take the higher score, otherwise, just add it.
+                //Also, you need to keep individual statistics for each user -the total points of all contests.
+
+                //Nested If statements
+                if (contests.ContainsKey(contestName))
+                {
+                    if (contests[contestName].Keys.Contains(username))
+                    {
+                        if (contests[contestName][username] < points)
+                        {
+                            contests[contestName][username] = points;
+                        }
+                    }
+                    else
+                    {
+                        contests[contestName].Add(username, points);
+                    }
+                }
+                else
+                {
+                    contests[contestName] = new Dictionary<string, int> { { username, points } };
+                }
+            }
+
+            //At that point, you should print each contest in order of input, for each contest print the participants ordered by points in descending order, then ordered by name in ascending order.
+            //•	The output format for the contests is:
+            //"{constestName}: {participants.Count} participants"
+            //"{position}. {username} <::> {points}"
+            //•	After you print all contests, print the individual statistics for every participant.
+            //•	The output format is:
+            
+
+            foreach (var contest in contests)
+            {
+                Console.WriteLine($"{contest.Key}: {contest.Value.Keys.Count} participants");
+
+                //create variables for positioncounter. It increases after each contest get printed
+                int counter = 1;
+                foreach (var participants in contest.Value.OrderByDescending(s => s.Value).ThenBy(name => name.Key))
+                {
+                    Console.WriteLine($"{counter}. {participants.Key} <::> {participants.Value}");
+                    counter++;
+                }
+            }
+            //After that, you should print individual statistics for every participant ordered by total points in descending order, and then by alphabetical order.
+
+            foreach (var contest in contests)
+            {
+                foreach (var participant in contest.Value)
+                {
+                    if (!totalPoints.ContainsKey(participant.Key))
+                    {
+                        totalPoints[participant.Key] = 0;
+                    }
+                    totalPoints[participant.Key] += participant.Value;
+                }
+            }
+            //"Individual standings:"
+            //"{position}. {username} -> {totalPoints}"
+            Console.WriteLine("Individual standings:");
+            int counterPosition = 1;
+            foreach (var user in totalPoints.OrderByDescending(s=>s.Value).ThenBy(k => k.Key))
+            {
+                Console.WriteLine($"{counterPosition}. {user.Key} -> {user.Value}");
+                counterPosition++;
+            }
+        }
+    }
+}
