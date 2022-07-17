8213f87cbd3384b585fac079c65b01bfb63f3ef4
achkatam <100487082+achkatam@users.noreply.github.com>
2022-07-17 11:25:04 -0400

    Added more solutions

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Text Processing More Exercises/04. Morse Code Translator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _04._Morse_Code_Translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that translates messages from Morse code to English (capital letters). Use this page to help you (without the numbers). The words will be separated by a space (' '). There will be a '|' character which you should replace with ' ' (space).
+
+
+            Dictionary<char, string> morseCodeAlphabet = new Dictionary<char, string>
+            {
+                ['A'] = ".-",
+                ['B'] = "-...",
+                ['C'] = "-.-.",
+                ['D'] = "-..",
+                ['E'] = ".", ['F'] = "..-.", ['G'] = "--.", ['H'] = "....", ['I'] = "..", ['J'] =".---", ['K'] = "-.-",
+                ['L'] = ".-..", ['M'] = "--", ['N'] = "-.", ['O'] = "---", ['P'] = ".--.", ['Q'] = "--.-", ['R'] = ".-.",
+                ['S'] = "...", ['T'] = "-", ['U'] = "..-", ['V'] = "...-", ['W'] = ".--", ['X'] = "-..-", ['Y'] = "-.--", ['Z'] = "--.."
+            };
+
+            var input = Console.ReadLine().Split('|');
+
+            StringBuilder decryptMessage = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var morseCode = input[i].Split();
+
+                foreach (var letter in morseCode)
+                {
+                    decryptMessage.Append(morseCodeAlphabet.FirstOrDefault(x => x.Value == letter).Key);
+                }
+
+                decryptMessage.Append(' ');
+
+            }
+
+            Console.WriteLine(decryptMessage);
+        }
+    }
+}
---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Text Processing More Exercises/05. HTML/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _05._HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive 3 lines of input. On the first line, you will receive a title of an article. On the next line, you will receive the content of that article. On the next n lines until you receive "end of comments" you will get the comments about the article. Print the whole information in HTML format. The title should be in "h1" tag (<h1></h1>); the content in article tag (<article></article>); each comment should be in div tag (<div></div>). 
+
+            string title = Console.ReadLine();
+            string articleTag = Console.ReadLine();
+
+            Console.WriteLine($"<h1>\n\t{title}\n</h1>");
+            Console.WriteLine($"<article>\n\t{articleTag}\n</article>");
+
+            string comment = Console.ReadLine();
+
+            while (comment != "end of comments")
+            {
+                Console.WriteLine($"<div>\n\t{comment}\n</div>");
+
+                comment = Console.ReadLine();
+            }
+        }
+    }
+}
