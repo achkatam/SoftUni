cff55b3bfb70eb5ef804f68af9673d30ca77c05c
achkatam <100487082+achkatam@users.noreply.github.com>
2022-07-25 20:11:35 -0400

    Added two solution from More Exercises

---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Regular Expressions More Exercises/1. Winning Ticket/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _1._Winning_Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //. The winning symbols are '@', '#', '$' and '^'. 
+            string winningPattern = @"(\@{6,10}|\${6,10}|\#{6,10}|\^{6,10})";
+
+            //You are given a collection of tickets separated by commas and spaces. You need to check every one of them if it has a winning combination of symbols.
+
+            var tickets = Console.ReadLine().Split(new string[] { ", ", " " }
+            , StringSplitOptions.RemoveEmptyEntries);
+            foreach (var ticket in tickets)
+            {
+                if (ticket.Length != 20)
+                {
+                    Console.WriteLine($"invalid ticket");
+                }
+                else
+                {
+                    //should uninterruptedly repeat at least 6 times in both the tickets left half and the tickets right half. 
+                    var firstHalfMatch = Regex.Match(ticket.Substring(0, 10), winningPattern);
+                    var secondHalfMatch = Regex.Match(ticket.Substring(10, 10), winningPattern);
+
+                    if (firstHalfMatch.Success && secondHalfMatch.Success)
+                    {
+                        //Find the shortest sequence of equal symbols
+                        int winLength = Math.Min(firstHalfMatch.Value.Length, secondHalfMatch.Value.Length);
+
+                        var leftSide = firstHalfMatch.Value.Substring(0, winLength);
+                        var rightSide = secondHalfMatch.Value.Substring(0, winLength);
+
+                        if (leftSide == rightSide)
+                        {
+                            //If you have 10 sequences in both side that is the Jackpot
+                            if (leftSide.Length == 10)
+                            {
+                                Console.WriteLine($"ticket \"{ticket}\" - {leftSide.Length}{leftSide[0]} Jackpot!");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"ticket \"{ticket}\" - {leftSide.Length}{leftSide[0]}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ticket \"{ticket}\" - no match");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{ticket}\" - no match");
+
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ C# Programming Fundamentals May 22'/C# Fundamentals May 22' - Regular Expressions More Exercises/2. Rage Quit/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+
+namespace _2._Rage_Quit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Every gamer knows what rage-quitting means. It’s basically when you’re just not good enough and you blame everybody else for losing a game. You press the CAPS LOCK key on the keyboard and flood the chat with gibberish to show your frustration.
+            //Chochko is a gamer and a bad one at that.He asks for your help; he wants to be the most annoying kid on his team, so when he rage - quits he wants something truly spectacular.He’ll give you a series of strings followed by non - negative numbers, e.g. "a3"; you need to print on the console each string repeated N times; convert the letters to uppercase beforehand. In the example, you need to write back "AAA".
+            //On the output, print first a statistic of the number of unique symbols used(the casing of letters is irrelevant, meaning that 'a' and 'A' are the same); the format should be "Unique symbols used {0}".Then, print the rage message itself.
+            //The strings and numbers will not be separated by anything. The input will always start with a string and for each string, there will be a corresponding number.The entire input will be given on a single line; Chochko is too lazy to make your job easier.
+
+            string pattern = @"([\D]+)([0-9]+)";
+
+            string input = Console.ReadLine();
+
+            MatchCollection inputMatch = Regex.Matches(input, pattern);
+
+            var word = new StringBuilder();
+
+            foreach (Match match in inputMatch)
+            {
+                var letters = match.Groups[1].Value;
+                int digits = int.Parse(match.Groups[2].Value);
+
+
+                for (int i = 0; i < digits; i++)
+                {
+                    word.Append(letters.ToString().ToUpper());
+                }
+
+            }
+
+            var symbols = new List<char>();
+
+            foreach (var ch in word.ToString())
+            {
+                if (!char.IsDigit(ch))
+                {
+                    if (!symbols.Contains(ch))
+                    {
+                        symbols.Add(ch);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Unique symbols used: {symbols.Count()}");
+            Console.WriteLine(word);
+        }
+    }
+}
