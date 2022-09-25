2e6646746fbea844e0806390ae268b3aaf0d296e
achkatam <agmatevv@gmail.com>
2022-09-25 11:53:20 -0400

    Added more solutions

---
+++ C# Advanced 22'/Streams-Files and Directories/Lab/05. Extract Special Bytes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Net.Http.Headers;
+
+namespace ExtractBytes
+{
+    public class ExtractBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\example.png";
+            string bytesFilePath = @"..\..\..\bytes.txt";
+            string outputPath = @"..\..\..\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            using (FileStream image = new FileStream(binaryFilePath, FileMode.Open))
+            {
+                using (FileStream bytes = new FileStream(bytesFilePath, FileMode.Open))
+                {
+                    byte[] bufferBytes = new byte[bytes.Length];
+
+                    bytes.Read(bufferBytes, 0, (int)bytes.Length);
+
+                    byte[] bufferImage = new byte[bytes.Length];
+
+                    bytes.Read(bufferImage, 0, (int)bytes.Length);
+
+                    using (FileStream output = new FileStream(outputPath, FileMode.Create))
+                    {
+                        for (int i = 0; i < bufferImage.Length; i++)
+                        {
+                            for (int j = 0; j < bufferBytes.Length; j++)
+                            {
+                                if (bufferImage[i] == bufferBytes[j])
+                                {
+                                    output.Write(new byte[] { bufferImage[i] });
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ C# Advanced 22'/Streams-Files and Directories/Lab/07. Folder Size/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"..\..\..\TestFolder";
+            string outputPath = @"..\..\..\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            var folderInfo = new DirectoryInfo(folderPath);
+            FileInfo[] files = folderInfo.GetFiles("*", SearchOption.AllDirectories);
+
+            double size = 0;
+            foreach (var file in files)
+            {
+                size += file.Length;
+            }
+
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                writer.Write(size / 1024);
+            }
+        }
+    }
+}
