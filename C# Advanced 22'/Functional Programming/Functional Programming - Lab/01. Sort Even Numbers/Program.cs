2025b06c1c824f7933fb7da420c186b4570abfbc
achkatam <agmatevv@gmail.com>
2022-09-27 13:09:36 -0400

    Added some solutions for Functional Programming - Lab

---
+++ C# Advanced 22'/Functional Programming/Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that reads one line of integers separated by ", ". Then prints the even numbers of that sequence sorted in increasing order.
+            Console.WriteLine(String.Join(", ",
+            Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToArray()));
+        }
+    }
+}
---
+++ C# Advanced 22'/Functional Programming/Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that reads a line of integers separated by ", ". Print on two lines the count of numbers and their sum.
+
+            //our parsing function
+            Func<string, int> parser = n => int.Parse(n);
+            int[] nums = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(nums.Length);
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
