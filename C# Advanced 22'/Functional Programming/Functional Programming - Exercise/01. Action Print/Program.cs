c8c6052f41ba1e17eb2d1bbb120c5de87b49fbbd
achkatam <agmatevv@gmail.com>
2022-09-30 09:57:15 -0400

    added some solutions

---
+++ C# Advanced 22'/Functional Programming/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that reads a collection of strings from the console and then prints them onto the console. Each name should be printed on a new line. Use Action<T>.
+
+            //input
+            var input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> print = input => Console.WriteLine(input) ;
+
+            foreach (var name in input)
+            {
+                print(name);
+            }
+        }
+
+    }
+}
---
+++ C# Advanced 22'/Functional Programming/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that reads a collection of names as strings from the console, appends "Sir" in front of every name and prints it back in the console. Use Action<T>.
+
+            Action<string> print = input => Console.WriteLine("Sir" + " " + input);
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var name in input)
+            {
+                print(name);
+            }
+        }
+    }
+}
