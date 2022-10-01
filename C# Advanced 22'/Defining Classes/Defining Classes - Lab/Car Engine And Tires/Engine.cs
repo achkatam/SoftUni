3a3747c35d2e292630f6aacdea32cdeb92968119
achkatam <agmatevv@gmail.com>
2022-10-01 12:52:11 -0400

    added solution

---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Lab/Car Engine And Tires/Engine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        private int horsepower;
+        private double cubicCapacity;
+
+        public int HorsePower
+        {
+            get { return horsepower; }
+            set { horsepower = value; }
+        }
+
+        public double CubicCapacity
+        {
+            get { return cubicCapacity; }
+            set { cubicCapacity = value; }
+        }
+
+        public Engine(int horsepower, double cubicCapacity)
+        {
+            HorsePower = horsepower;
+            CubicCapacity = cubicCapacity;
+        }
+
+    }
+}
---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Lab/Car Engine And Tires/Tire.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+		public int Year
+		{
+			get { return year; }
+			set { year = value; }
+		}
+
+		public double Pressure
+		{
+			get { return pressure; }
+			set { pressure = value; }
+		}
+
+        public Tire(int year, double pressure)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+    }
+}
