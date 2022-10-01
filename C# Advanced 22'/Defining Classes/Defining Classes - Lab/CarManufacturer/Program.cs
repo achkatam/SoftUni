865be72bb64e11203d9baa360eec6d38f61352fd
achkatam <agmatevv@gmail.com>
2022-10-01 11:58:16 -0400

    Added solution

---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Lab/CarManufacturer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Make = "BMW",
+                Model = "748i",
+                Year = 2001,
+                FuelConsumption = 0.9,
+                FuelQuantity = 100
+            };
+
+            while (true)
+            {
+                Console.WriteLine($"Where to go?");
+                car.Drive(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine(car.WhoAmI());
+            }
+        }
+    }
+}
