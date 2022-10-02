43a93b14d8e76b607c4864808df165375883e979
achkatam <agmatevv@gmail.com>
2022-10-01 23:45:15 -0400

    added solution

---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Lab/Special car/Car.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        //1 Fields
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+        //2. Properties
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                this.year = value;
+            }
+        }
+
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        //3. Constructors
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires) : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+        }
+
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+
+
+        public Tire[] Tires
+        {
+            get { return tires; }
+            set { tires = value; }
+        }
+
+        //4. Methods
+        public void Drive(double distance)
+        {
+            double fuelNeeded = FuelConsumption * distance / 100;
+
+            if (fuelNeeded >= FuelQuantity)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                FuelQuantity -= fuelNeeded;
+            }
+        }
+
+        public override string ToString() => $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nHorsePowers: {this.Engine.HorsePower}\nFuelQuantity: {this.FuelQuantity}";
+
+
+        //public string WhoAmI()
+        //{
+        //    return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        //}
+    }
+}
---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Lab/Special car/StartUp.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Tire[]> tires = GetTiresInfo();
+            List<Engine> engines = GetEnginesInfo();
+            List<Car> cars = GetCarsInfo(tires, engines);
+
+            var specialCar = cars.FindAll(c => c.Year >= 2017
+            && c.Engine.HorsePower > 330
+            && c.Tires.Sum(x => x.Pressure) >= 9
+            && c.Tires.Sum(x => x.Pressure) <= 10)
+                .ToList();
+
+            specialCar.ForEach(c => c.Drive(20));
+            specialCar.ForEach(c => Console.WriteLine(c));
+
+
+            //foreach (var car in specialCar)
+            //{
+            //    car.Drive(20);
+            //    Console.WriteLine(car);
+            //}
+        }
+
+        static List<Car> GetCarsInfo(List<Tire[]> tires, List<Engine> engines)
+        {
+            List<Car> cars = new List<Car>();
+
+            string command = Console.ReadLine();
+
+            while (command != "Show special")
+            {
+                var tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string make = tokens[0];
+                string model = tokens[1];
+                int year = int.Parse(tokens[2]);
+                double fuelQuantity = double.Parse(tokens[3]);
+                double fuelConsumption = double.Parse(tokens[4]);
+                var engine = int.Parse(tokens[5]);
+                var tire = int.Parse(tokens[6]);
+
+                Car car = new Car(make, model, year, fuelQuantity, fuelConsumption, engines[engine], tires[tire]);
+
+                cars.Add(car);
+
+                command = Console.ReadLine();
+            }
+
+
+            return cars;
+        }
+
+        static List<Engine> GetEnginesInfo()
+        {
+            List<Engine> engines = new List<Engine>();
+
+            string command = Console.ReadLine();
+
+            while (command != "Engines done")
+            {
+                var tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int horsepower = int.Parse(tokens[0]);
+                double cubicCapacity = double.Parse(tokens[1]);
+
+                Engine engine = new Engine(horsepower, cubicCapacity);
+
+                engines.Add(engine);
+
+                command = Console.ReadLine();
+            }
+
+            return engines;
+        }
+
+        static List<Tire[]> GetTiresInfo()
+        {
+            List<Tire[]> tires = new List<Tire[]>();
+
+            string command = Console.ReadLine();
+
+            while (command != "No more tires")
+            {
+                var tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Tire[] tire = new Tire[4]
+                {
+                    new Tire(int.Parse(tokens[0]), double.Parse(tokens[1])),
+                    new Tire(int.Parse(tokens[2]), double.Parse(tokens[3])),
+                    new Tire(int.Parse(tokens[4]), double.Parse(tokens[5])),
+                    new Tire(int.Parse(tokens[6]), double.Parse(tokens[7])),
+                };
+
+                tires.Add(tire);
+
+                command = Console.ReadLine();
+            }
+
+            return tires;
+        }
+    }
+}
