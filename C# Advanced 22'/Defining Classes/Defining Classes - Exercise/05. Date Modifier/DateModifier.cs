76bc9cd9914632ff3a7b3a930fdbe9a47608630c
achkatam <agmatevv@gmail.com>
2022-10-03 16:01:46 -0400

    added solution

---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public static int DateTimeDifference(string startDate, string endDate)
+        {
+            DateTime firstDate = DateTime.Parse(startDate);
+            DateTime secDate = DateTime.Parse(endDate);
+
+            TimeSpan diff = firstDate - secDate;
+
+            return Math.Abs(diff.Days);
+        }
+    }
+}
---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secDate = Console.ReadLine();
+
+            int diff = DateModifier.DateTimeDifference(firstDate, secDate);
+
+            Console.WriteLine(diff);
+        }
+    }
+}
