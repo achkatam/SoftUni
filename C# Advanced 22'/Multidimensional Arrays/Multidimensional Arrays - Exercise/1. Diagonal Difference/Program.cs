9855dc09607528f06d4c0fb6948729965fa59bb9
achkatam <agmatevv@gmail.com>
2022-10-03 10:54:02 -0400

    solutions updated

--- C# Advanced 22'/Multidimensional Arrays/Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
+++ C# Advanced 22'/Multidimensional Arrays/Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -1,71 +1,67 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
+using System.Data;
 using System.Linq;
 
 namespace _1._Diagonal_Difference
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-
             //Create a program that finds the difference between the sums of the square matrix diagonals (absolute value).
 
-            int n = int.Parse(Console.ReadLine());
+            int size = int.Parse(Console.ReadLine());
 
-            //create the matrix
-            int[,] matrix = new int[n, n];
+            int[,] matrix = new int[size, size];
 
-            //Method for matrix data typing in
             MatrixData(matrix);
 
-            //variable for sum of primary diagonal
-            int primarySum = 0;
-            ////secondary sum
+            //sum up the primary diagonal
+            int primarySum = PrimaryDiagonal(matrix);
+
+            //sum up the secondary diagonal
+            int secSum = SecondaryDiagonal(matrix);
+
+            //PrintOutput
+            Console.WriteLine(Math.Abs(primarySum - secSum));
+        }
+
+        static int SecondaryDiagonal(int[,] matrix)
+        {
             int secSum = 0;
 
-            for (int row = 0, col = matrix.GetLength(0) - 1; row < matrix.GetLength(0); row++, col--)
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                primarySum += matrix[row, row];
-                secSum += matrix[row, col];
+                secSum += matrix[row, matrix.GetLength(0) - row - 1];
             }
 
-            //for (int row = 0; row < matrix.GetLength(0); row++)
-            //{
-            //    primarySum += matrix[row, row];
-            //}
+            return secSum;
+        }
 
-            ////Console.WriteLine(primarySum);
+        static int PrimaryDiagonal(int[,] matrix)
+        {
+            int primarySum = 0;
 
-            
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                primarySum += matrix[row, row];
+            }
 
-            //for (int row = 0; row < matrix.GetLength(0); row++)
-            //{
-            //    secSum += matrix[row, matrix.GetLength(0) - row - 1];
-            //}
-
-            //Console.WriteLine(secSum);
-
-            //variable for absolute difference primarySum - secSum
-            int difference = Math.Abs(primarySum - secSum);
-
-            //Print output
-            Console.WriteLine(difference);
-
+            return primarySum;
         }
 
         static void MatrixData(int[,] matrix)
         {
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                var colInfo = Console.ReadLine()
+                int[] rowData = Console.ReadLine()
                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                    .Select(n => int.Parse(n))
+                    .Select(int.Parse)
                     .ToArray();
 
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    matrix[row, col] = colInfo[col];
+                    matrix[row, col] = rowData[col];
                 }
             }
         }
--- C# Advanced 22'/Multidimensional Arrays/Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
+++ C# Advanced 22'/Multidimensional Arrays/Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
@@ -1,63 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 namespace _2._Squares_in_Matrix
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            //Find the count of 2 x 2 squares of equal chars in a matrix.
+            //Input
+            //•	On the first line, you are given the integers rows and cols – the matrix's dimensions.
+            //•	Matrix characters come at the next rows lines(space separated).
 
-            //matrix's dimension
-            var input = Console.ReadLine()
+            int[] size = Console.ReadLine()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
 
-            //create the matrix
-            char[,] matrix = new char[input[0], input[1]];
+            int rows = size[0];
+            int cols = size[1];
 
-             MatrixData(matrix);
+            char[,] matrix = new char[rows, cols];
 
-            //counter. Increases if there is/are a/ square/s in the matrix
+            MatrixData(matrix);
+
+            //counter for square matrix
+            int counter = GetSquareMatrix(matrix);
+
+            Console.WriteLine(counter);
+        }
+
+        static int GetSquareMatrix(char[,] matrix)
+        {
             int counter = 0;
 
-            if (matrix.GetLength(0) > 1
-                && matrix.GetLength(1) > 1)
-            {
+            if (SizeIsGood(matrix))
+                //iterate to the last element of rows
                 for (int row = 0; row < matrix.GetLength(0) - 1; row++)
                 {
+                    //iterate to the last element of cols
                     for (int col = 0; col < matrix.GetLength(1) - 1; col++)
                     {
-                        if (matrix[row, col] == matrix[row, col + 1]
-                            && matrix[row, col] == matrix[row + 1, col + 1]
-                            && matrix[row, col] == matrix[row + 1, col])
-                        {
-                            counter++;
-                        }
+                        bool squareFound = FindSquare(matrix, row, col);
+
+                        if (squareFound) counter++;
                     }
                 }
 
-                Console.WriteLine(counter);
-            }
-
-            //matrix[row, col] == matrix[row, col + 1]
-            //&& matrix[row, col] == matrix[row + 1, col + 1]
-            //&& matrix[row, col] == matrix[row + 1, col])
-            
+            return counter;
         }
+
+        static bool FindSquare(char[,] matrix, int row, int col)
+        {
+            return
+                //equals the element on right side
+                matrix[row, col] == matrix[row, col + 1]
+                            //bottom left
+                            && matrix[row, col] == matrix[row + 1, col]
+                            //bottom right
+                            && matrix[row, col] == matrix[row + 1, col + 1];
+        }
+
+        static bool SizeIsGood(char[,] matrix) => matrix.GetLength(0) > 1 && matrix.GetLength(1) > 1;
 
         static void MatrixData(char[,] matrix)
         {
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                var chars = Console.ReadLine()
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                    .Select(c => char.Parse(c))
+                char[] rowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
                     .ToArray();
+
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    matrix[row, col] = chars[col];
+                    matrix[row, col] = rowData[col];
                 }
             }
         }
