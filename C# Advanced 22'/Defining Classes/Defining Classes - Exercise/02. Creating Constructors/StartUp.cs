99452c2ce84f9c05cd14f2e17802039c0e1aa300
achkatam <agmatevv@gmail.com>
2022-10-03 15:11:19 -0400

    initial commit

---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //NOTE: You need a StartUp class with the namespace DefiningClasses.
+            //Define a class Person with private fields for name and age and public properties Name and Age.
+
+           
+        }
+    }
+}
