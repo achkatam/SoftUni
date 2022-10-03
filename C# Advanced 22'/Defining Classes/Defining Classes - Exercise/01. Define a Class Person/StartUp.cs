a06b32372653cc5e4c9e11fa4792747e0ca0a5da
achkatam <agmatevv@gmail.com>
2022-10-03 14:46:11 -0400

    initial commit

---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //NOTE: You need a StartUp class with the namespace DefiningClasses.
+            //Define a class Person with private fields for name and age and public properties Name and Age.
+
+            Person person = new Person();
+
+            person.Name = "Achkata";
+            person.Age = 25;
+
+
+            Console.WriteLine($"{person.Name} {person.Age}");
+        }
+    }
+}
