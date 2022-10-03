7d15666f4dfa1aeb39ac6a01a73014f57762c081
achkatam <agmatevv@gmail.com>
2022-10-03 16:42:46 -0400

    added solution

---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Exercise/06.Speed Racing/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace _06.Speed_Racing
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelPerKm)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelPerKm;
+            TravelledDistance = 0;
+        }
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; }
+
+        public override string ToString() => $"{Model} {FuelAmount:f2} {TravelledDistance}";
+
+        public void Drive(double distance)
+        {
+            //Implement a method in the Car class to calculate whether or not a car can move that distance. If it can, the car’s fuel amount should be reduced by the amount of used fuel and its traveled distance should be increased by the number of the traveled kilometers. Otherwise, the car should not move (its fuel amount and the traveled distance should stay the same) and you should print on the console:
+            //"Insufficient fuel for the drive"
+
+            double fuelNeeded = FuelConsumptionPerKilometer * distance;
+
+            if (fuelNeeded > FuelAmount)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+
+                return;
+            }
+
+            FuelAmount -= fuelNeeded;
+            TravelledDistance += distance;
+        }
+    }
+}
---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Exercise/06.Speed Racing/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _06.Speed_Racing
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           var cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                //"{model} {fuelAmount} {fuelConsumptionFor1km}"
+                var carInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = carInfo[0];
+                double fuelAmount = double.Parse(carInfo[1]);
+                double fuelConsumptionPerKm = double.Parse(carInfo[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionPerKm);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                var tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = tokens[1];
+                double distance = double.Parse(tokens[2]);
+
+                Car car = cars.Find(x => x.Model == model);
+
+                car.Drive(distance);
+
+                command = Console.ReadLine();
+            }
+
+            cars.ForEach(car => Console.WriteLine(car));
+
+        }
+    }
+}
