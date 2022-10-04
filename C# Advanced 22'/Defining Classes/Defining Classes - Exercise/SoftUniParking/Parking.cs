66ad765b6dad697ab75f50b7486c1ac44a71282a
achkatam <agmatevv@gmail.com>
2022-10-03 22:57:22 -0400

    added solution

---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Exercise/SoftUniParking/Parking.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private int capacity;
+        private Dictionary<string, Car> cars;
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            this.cars = new Dictionary<string, Car>();
+        }
+
+        public int Count { get { return this.cars.Count; } }
+
+        public string AddCar(Car car)
+        {
+            if (this.cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (this.cars.Count == this.capacity)
+            {
+                return "Parking is full!";
+            }
+
+            this.cars.Add(car.RegistrationNumber, car);
+
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!this.cars.ContainsKey(registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            cars.Remove(registrationNumber);
+
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return this.cars[registrationNumber];
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var licensePlate in registrationNumbers)
+            {
+                this.RemoveCar(licensePlate);
+            }
+        }
+
+
+
+        //Make: Skoda
+        //Model: Fabia
+        //HorsePower: 65
+        //RegistrationNumber: CC1856BG
+    }
+}
