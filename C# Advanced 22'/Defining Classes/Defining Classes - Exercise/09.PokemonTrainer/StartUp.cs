8b0e1ec164f951f613d9b595813953603e5574c4
achkatam <agmatevv@gmail.com>
2022-10-04 16:14:24 -0400

    Added solution

---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Exercise/09.PokemonTrainer/StartUp.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace _09.PokemonTrainer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var trainers = new List<Trainer>();
+
+            string command = Console.ReadLine();
+
+            while (command != "Tournament")
+            {
+                //"{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
+                var tokens = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var trainerName = tokens[0];
+
+                //Pokemon's data
+                var pokemonName = tokens[1];
+                string element = tokens[2];
+                int hp = int.Parse(tokens[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, element, hp);
+
+                var currTrainer = trainers.FirstOrDefault(x => x.Name == trainerName);
+
+                if (currTrainer != null)
+                {
+                    currTrainer.Pokemons.Add(pokemon);
+                }
+                else
+                {
+                    var trainer = new Trainer(trainerName);
+
+                    trainer.Pokemons.Add(pokemon);
+
+                    trainers.Add(trainer);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                CheckMethods(trainers, command);
+
+                command = Console.ReadLine();
+            }
+
+            PrintOutput(trainers);
+
+        }
+
+        static void PrintOutput(List<Trainer> trainers)
+        {
+            foreach (var trainer in trainers.OrderByDescending(x => x.NumberOfCollection))
+            {
+                Console.WriteLine(trainer);
+            }
+
+        }
+
+        static void CheckMethods(List<Trainer> trainers, string command)
+        {
+            string element = command;
+
+            foreach (var trainer in trainers)
+            {
+                if (trainer.Pokemons.FirstOrDefault(p => p.Element == element) != null)
+                {
+                    trainer.AddBadge();
+                }
+                else
+                {
+                    trainer.ReducePokemonHp();
+                }
+
+                trainer.ClearDeadOnes();
+            }
+        }
+
+    }
+}
---
+++ C# Advanced 22'/Defining Classes/Defining Classes - Exercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+
+        }
+        public string Name { get; set; }
+        public int NumberOfCollection { get; set; }
+        public List<Pokemon> Pokemons = new List<Pokemon>();
+
+        public void AddBadge()
+        {
+            NumberOfCollection++;
+        }
+
+        public void ReducePokemonHp()
+        {
+            foreach (var pokemon in Pokemons)
+            {
+                //all of his pokemon lose 10 health
+                pokemon.Health -= 10;
+            }
+        }
+
+        public void ClearDeadOnes()
+        {
+
+            //he dies and must be deleted 
+            Pokemons.RemoveAll(x => x.Health <= 0);
+        }
+
+        public override string ToString() => $"{Name} {NumberOfCollection} {Pokemons.Count}";
+
+    }
+}
