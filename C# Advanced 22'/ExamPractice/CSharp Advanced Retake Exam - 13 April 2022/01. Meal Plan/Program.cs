51a51fa3189104c0e825a13ffe09f1934645a3d1
achkatam <agmatevv@gmail.com>
2022-10-08 15:05:20 -0400

    Added some solutions

---
+++ C# Advanced 22'/ExamPractice/CSharp Advanced Retake Exam - 13 April 2022/01. Meal Plan/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace _01._Meal_Plan
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input for mealsInfo
+            var mealsnInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            // create Queue<string> of meals
+            Queue<string> meals = new Queue<string>(mealsnInfo);
+
+            //Input for caloriesInfo
+            var caloriesInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            // create Stack<int> of cals
+            Stack<int> calories = new Stack<int>(caloriesInfo);
+
+            //salad	350
+            //soup    490
+            //pasta   680
+            //steak   790
+
+
+            //Create dictionary<meal, cals>
+            Dictionary<string, int> mealsDict = new Dictionary<string, int>()
+            {
+                {"salad", 350 },
+                {"soup", 490 },
+                {"pasta", 680 },
+                {"steak", 790 },
+            };
+
+            //counter for eaten meals, will need it for the output
+            int eatenMeals = 0;
+
+            while (meals.Any() && calories.Any())
+            {
+                //take the first meal’s calories and the last element in the sequence of calorie intake for a day
+                int dailyCals = calories.Pop();
+
+                //, the calories intake for the current day should be decreased by the calories of the given meal, and the current meal should be removed 
+                while (meals.Any() && dailyCals > 0)
+                {
+                    string currMeal = meals.Dequeue();
+                    dailyCals -= mealsDict[currMeal];
+                    eatenMeals++;
+                }
+
+                //When the calories intake for the given day goes to zero, remove them from the collection
+                if (dailyCals == 0)
+                {
+                    dailyCals = 0;
+                }
+                else if (dailyCals < 0)
+                {
+                    //given day go below zero, take as many calories as possible from the meal, so the current daily calories intake reaches zero
+                    if (calories.Any())
+                    {
+                        var nextDayCals = calories.Pop() + dailyCals;
+
+                        calories.Push(nextDayCals);
+                    }
+                }
+
+                //if there is no meals for the day but there is still calories left, push them in the stack of cals
+                if (!meals.Any() && dailyCals > 0)
+                {
+                    calories.Push(dailyCals);
+                }
+            }
+
+
+            PrintOutput(calories, meals, eatenMeals);
+
+        }
+
+        public static void PrintOutput(Stack<int> calories, Queue<string> meals, int eatenMeals)
+        {
+            //o	If John manage to eat all the given meals print:
+            if (!meals.Any())
+            {
+                Console.WriteLine($"John had {eatenMeals} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", calories)} calories.");
+            }
+            else
+            {
+                Console.WriteLine($"John ate enough, he had {eatenMeals} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", meals)}.");
+            }
+            //            "John had {number of meals} meals."
+            //"For the next few days, he can eat {dailyCalories1, dailyCalories2, etc.} calories."
+            //Separate the daily calories intake by comma and space(", ").
+            //o If John could not eat every meal:
+            //"John ate enough, he had {number of meals} meals."
+            //"Meals left: {meal1, meal2, etc.}."
+
+        }
+    }
+}
---
+++ C# Advanced 22'/ExamPractice/CSharp Advanced Retake Exam - 13 April 2022/02. Truffle Hunter/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace _02._Truffle_Hunter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //dict of type of truffle and its occurence
+            var truffles = new Dictionary<char, int>()
+            {
+                {'S',0 },
+                {'W',0 },
+                {'B',0 }
+            };
+
+            int size = int.Parse(Console.ReadLine());
+
+            var matrix = new char[size, size];
+
+            CreateMatrix(matrix);
+
+            //eaten truffles by the wild boar
+            int counter = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Stop the hunt") break;
+
+                var tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string cmd = tokens[0];
+
+                switch (cmd)
+                {
+                    case "Collect":
+                        CollectTruffles(matrix, truffles, tokens);
+                        break;
+                    case "Wild_Boar":
+                        counter = BoarMovement(matrix, tokens, truffles, counter);
+                        break;
+                }
+            }
+
+            PrintOutput(matrix, truffles, counter);
+
+        }
+
+        public static bool IsValidPosition(int row, int col, char[,] forest)
+        {
+            return row >= 0 &&
+                   col >= 0 &&
+                   row < forest.GetLength(0) &&
+                   col < forest.GetLength(1);
+        }
+        static void PrintOutput(char[,] matrix, Dictionary<char, int> truffles, int counter)
+        {
+            Console.WriteLine($"Peter manages to harvest {truffles['B']} black, {truffles['S']} summer, and {truffles['W']} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {counter} truffles.");
+            //Then print the last state of the forest.
+            PrintMatrix(matrix);
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static int BoarMovement(char[,] matrix, string[] tokens, Dictionary<char, int> truffles, int counter)
+        {
+            int row = int.Parse(tokens[1]);
+            int col = int.Parse(tokens[2]);
+
+            string direction = tokens[3];
+
+
+            if (matrix[row, col] != '-')
+            {
+                counter++;
+                matrix[row, col] = '-';
+            }
+
+            while (true)
+            {
+                switch (direction)
+                {
+                    case "up"://rows -= 2
+                        row -= 2;
+                        break;
+                    case "down": // rows += 2
+                        row += 2;
+                        break;
+                    case "left": // col -=2
+                        col -= 2;
+                        break;
+                    case "right": //col += 2
+                        col += 2;
+                        break;
+                }
+
+                if (!IsValidPosition(row, col, matrix))
+                {
+                    break;
+                }
+                else if (matrix[row, col] != '-')
+                {
+                    counter++;
+                    matrix[row, col] = '-';
+                }
+            }
+
+
+            return counter;
+        }
+
+        static void CollectTruffles(char[,] matrix, Dictionary<char, int> truffles, string[] tokens)
+        {
+            int row = int.Parse(tokens[1]);
+            int col = int.Parse(tokens[2]);
+
+            if (matrix[row, col] != '-')
+            {
+                char truffleType = matrix[row, col];
+                truffles[truffleType]++;
+
+                matrix[row, col] = '-';
+            }
+
+        }
+
+        static void CreateMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+        }
+    }
+}
