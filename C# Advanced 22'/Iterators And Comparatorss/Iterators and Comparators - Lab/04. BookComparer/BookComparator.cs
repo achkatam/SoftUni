5e8c2b29196cc9f2b86c5ce1ac34771fd5905054
achkatam <agmatevv@gmail.com>
2022-10-15 09:33:37 -0400

    Added solution

---
+++ C# Advanced 22'/Iterators And Comparatorss/Iterators and Comparators - Lab/04. BookComparer/BookComparator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare( Book firstBook, Book secondBook)
+        {
+            int result = firstBook.Title.CompareTo(secondBook.Title);
+            if (result == 0)
+            {
+                if (firstBook.Year > secondBook.Year)
+                {
+                    return -1;
+                }
+                else if (firstBook.Year < secondBook.Year)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            return result;
+        }
+    }
+}
