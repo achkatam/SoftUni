9aee3c02993e3d2601a381c2e4d76d3a9ea44887
achkatam <agmatevv@gmail.com>
2022-10-15 08:57:09 -0400

    added one more solution

---
+++ C# Advanced 22'/Iterators And Comparatorss/Iterators and Comparators - Lab/01. Library/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors;
+        }
+    }
+}
