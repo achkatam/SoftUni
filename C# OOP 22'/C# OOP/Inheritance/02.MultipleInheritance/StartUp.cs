cb5b3f80e20e4a52d76a537dc5be4f92d468bfc5
achkatam <agmatevv@gmail.com>
2022-10-25 14:31:56 -0400

    Added first lecture from OOP

---
+++ C# OOP 22'/C# OOP/Inheritance/02.MultipleInheritance/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+
+        }
+    }
+}
---
+++ C# OOP 22'/C# OOP/Inheritance/04.RandomList/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList myList = new RandomList();
+
+            myList.Add("Hello");
+            myList.Add("World");
+
+            Console.WriteLine(myList.RandomString());
+        }
+    }
+}
