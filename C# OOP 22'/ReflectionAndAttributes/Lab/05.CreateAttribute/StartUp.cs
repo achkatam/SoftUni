6874d61f871b866e6624e9160ef42ff63fbb246b
achkatam <agmatevv@gmail.com>
2022-11-17 21:21:30 -0500

    Added Collector solution

---
+++ C# OOP 22'/ReflectionAndAttributes/Lab/05.CreateAttribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthorProblem
+{
+    using System;
+
+    [Author("Angel")]
+    public class StartUp
+    {
+        [Author("Kyle")]
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
