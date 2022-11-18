713e62fd3bc49226da35998724946514686778b5
achkatam <agmatevv@gmail.com>
2022-11-17 21:19:33 -0500

    Added solution for Code Tracker

---
+++ C# OOP 22'/ReflectionAndAttributes/Lab/06.CodeTracker/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace AuthorProblem
+{
+    using System;
+
+    [Author("Angel")]
+    public class StartUp
+    {
+        [Author("Kyle")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+
+        [Author("Angel")]
+        public void PrintName()
+        {
+            Console.WriteLine("My name is Angel");
+        }
+
+        [Author("Smith")]
+        public void GreatJob()
+        {
+            Console.WriteLine("You're doing great job!!!");
+        }
+    }
+}
---
+++ C# OOP 22'/ReflectionAndAttributes/Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,35 @@
+﻿namespace AuthorProblem
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class Tracker
+    {
+        public Tracker()
+        {
+
+        }
+
+        public void PrintMethodsByAuthor()
+        {
+
+            Type type = typeof(StartUp);
+
+            MethodInfo[] methods = type.GetMethods((BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static));
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(m => m.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
