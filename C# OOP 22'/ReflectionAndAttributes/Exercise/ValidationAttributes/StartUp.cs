b0078d34df9bd5079f2efba69b3197fde942a6ca
achkatam <agmatevv@gmail.com>
2022-11-18 13:25:07 -0500

    Added one more solution. ReadMe fixed

---
+++ C# OOP 22'/ReflectionAndAttributes/Exercise/ValidationAttributes/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace ValidationAttributes
+{
+    using System;
+
+    using Models;
+    using Utilities;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person
+             (
+                 "Sarah Smith",
+                 19
+             );
+
+            bool isValidEntity = Validator.IsValid(person);
+
+            Console.WriteLine(isValidEntity);
+        }
+    }
+}
---
+++ C# OOP 22'/ReflectionAndAttributes/Exercise/ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,52 @@
+﻿namespace ValidationAttributes.Utilities
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using ValidationAttributes.Utilities.Attributes;
+
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type objType = obj.GetType();
+
+            PropertyInfo[] propsInfo = objType.GetProperties()
+                .Where(p => p.CustomAttributes.Any(ca => typeof(MyValidationAttribute).IsAssignableFrom(ca.AttributeType)))
+                .ToArray();
+
+            foreach (PropertyInfo validProp in propsInfo)
+            {
+                //CustomAttributeData[] customAttrs = validProp.CustomAttributes
+                //    .ToArray();
+                object[] customAttributes = validProp.GetCustomAttributes()
+                    .Where(ca => typeof(MyValidationAttribute).IsAssignableFrom(ca.GetType()))
+                    .ToArray();
+
+                object propValue = validProp.GetValue(obj);
+
+                foreach (object customAttr in customAttributes)
+                {
+
+                    MethodInfo isValidMethod = customAttr.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                        .FirstOrDefault(mi => mi.Name == "IsValid");
+
+                    if (isValidMethod == null)
+                    {
+                        throw new InvalidOperationException("Your custom attribute doesn't have IsValid method!");
+                    }
+
+
+                    bool result = (bool)isValidMethod.Invoke(customAttr, new object[] { propValue });
+
+                    if (!result)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
