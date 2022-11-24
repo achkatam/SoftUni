c98f782d39a193deb9046a3e8b4082d47a3bf1f0
achkatam <agmatevv@gmail.com>
2022-11-23 22:12:15 -0500

    CarManager fixed 100/100

--- C# OOP 22'/UnitTesting/UnitTesting - Exercise/CarManager.Tests/CarManagerTests.cs
+++ C# OOP 22'/UnitTesting/UnitTesting - Exercise/CarManager.Tests/CarManagerTests.cs
@@ -1,8 +1,9 @@
-namespace CarManager.Tests
+﻿namespace CarManager.Tests
 {
-    using System;
-
     using NUnit.Framework;
+    using System;
+    using System.Linq;
+    using System.Reflection;
 
     [TestFixture]
     public class CarManagerTests
@@ -10,131 +11,158 @@ namespace CarManager.Tests
         private Car car;
 
         [SetUp]
-        public void SetUp()
+        public void Setup()
         {
-            car = new Car("Audi", "A8", 3.4, 70);
+            car = new Car("Ferrari", "F40", 23, 120);
         }
 
         [Test]
-        public void Test_Constructor_Initialize_Car_Correctly()
+        public void TEST_CONSTRUCTOR_SOULD_SET_FUELAMOUNT_AT_ZERO()
         {
-            //Arrange
-            //Act
-            car = new Car("Audi", "A8", 3.4, 70);
+            car = new Car("Ferrari", "F40", 23, 120);
 
-            //Assert
-            Assert.AreEqual(car.Make, "Audi");
-            Assert.AreEqual(car.Model, "A8");
-            Assert.AreEqual(car.FuelConsumption, 3.4);
-            Assert.AreEqual(car.FuelCapacity, 70);
-            Assert.AreEqual(car.FuelAmount, 0);
+            double expected = 0;
+            double actual = car.FuelAmount;
+
+            Assert.AreEqual(expected, actual);
         }
 
         [TestCase(null)]
         [TestCase("")]
-        public void TEST_MAKE_SETTER_SHOULD_THROW_EXCEPTION_IF_NULL_OR_EMPTY(string make)
+        public void TEST_MAKE_SHOULD_THROW_EXCEPTION_IF_NULL_OR_EMPTY(string make)
         {
             Assert.Throws<ArgumentException>(() =>
             {
-                car = new Car(make, "A8", 3.4, 70);
-            }, "Model cannot be null or empty!");
-        }
-
-        [TestCase(null)]
-        [TestCase("")]
-        public void TEST_MODEL_SETTER_SHOULD_THROW_EXCEPTION_IF_NULL_OR_EMPTY(string model)
-        {
-            Assert.Throws<ArgumentException>(() =>
-            {
-                car = new Car("Audi", model, 3.4, 70);
+                car = new Car(make, "F40", 25, 120);
             }, "Make cannot be null or empty!");
         }
 
-        [TestCase(-2)]
-        [TestCase(-24)]
-        [TestCase(0)]
-        public void TEST_FUELCONSUMPTION_SETTER_SHOULD_THROW_EXCEPTION_IF_ZERO_OR_NEGATIVE(double fuelConsumption)
+        [TestCase(null)]
+        [TestCase("")]
+        public void TEST_MODEL_SHOULD_THROW_EXCEPTION_IF_NULL_OR_EMPTY(string model)
         {
             Assert.Throws<ArgumentException>(() =>
             {
-                car = new Car("Audi", "A8", fuelConsumption, 70);
+                car = new Car("Ferrari", model, 25, 120);
+            }, "Model cannot be null or empty!");
+        }
+
+        [TestCase(0)]
+        [TestCase(-2)]
+        [TestCase(-22)]
+        public void TEST_FUELCONSUMPTION_SHOULD_THROW_EXCEPTION_IF_NEGATIVE_OR_ZERO(double fuelConsumption)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                car = new Car("Ferrari", "F40", fuelConsumption, 120);
             }, "Fuel consumption cannot be zero or negative!");
         }
 
-        [TestCase(-2)]
-        [TestCase(-24)]
         [TestCase(0)]
-        public void TEST_FUEL_CAPACITY_SETTER_SHOULD_THROW_EXCEPTION_IF_ZERO_OR_NEGATIVE(double fuelCapacity)
+        [TestCase(-2)]
+        [TestCase(-22)]
+        public void TEST_FUELCAPACITY_SHOULD_THROW_EXCEPTION_IF_NEGATIVE_OR_ZERO(double fuelCapacity)
         {
             Assert.Throws<ArgumentException>(() =>
             {
-                car = new Car("Audi", "A8", 3.4, fuelCapacity);
+                car = new Car("Ferrari", "F40", 25, fuelCapacity);
             }, "Fuel capacity cannot be zero or negative!");
         }
 
-        [TestCase(-4)]
-        [TestCase(-42)]
+        [Test]
+        public void TEST_CONSTRUCTOR_SHOULD_RETURN_VALID_CAR()
+        {
+            car = new Car("Ferrari", "F40", 25, 120);
+
+            Assert.AreEqual(car.Make, "Ferrari");
+            Assert.AreEqual(car.Model, "F40");
+            Assert.AreEqual(car.FuelConsumption, 25);
+            Assert.AreEqual(car.FuelCapacity, 120);
+            Assert.AreEqual(car.FuelAmount, 0);
+        }
+
         [TestCase(0)]
-        public void TEST_REFUEL_AMOUNT_CANNOT_BE_NEGATIVE_OR_ZERO(double fuelToAdd)
+        [TestCase(-12)]
+        [TestCase(-1)]
+        public void TEST_REFUEL_METHOD_SHOULD_THROW_EXCEPTION_IF_NEGATIVE_OR_ZERO(double fuelToRefill)
         {
             Assert.Throws<ArgumentException>(() =>
             {
-                car.Refuel(fuelToAdd);
+                car.Refuel(fuelToRefill);
             }, "Fuel amount cannot be zero or negative!");
         }
 
-        [TestCase(11)]
-        [TestCase(22)]
-        [TestCase(26)]
-        public void TEST_REFUEL_AMOUNT_INCREASES_CAR_FUEL_AMOUNT(double fuelToRefuel)
+        [TestCase(20)]
+        [TestCase(12)]
+        [TestCase(10)]
+        public void TEST_REFUEL_METHOD_SHOULD_INCREASE_FUELAMOUNT(double fuelToRefill)
         {
-            car = new Car("Audi", "A8", 3.4, 70);
-            double beforeRefueling = car.FuelAmount;
+            car = new Car("Ferrari", "F40", 25, 120);
+            double beforeRefuel = car.FuelAmount;
 
-            car.Refuel(fuelToRefuel);
+            car.Refuel(fuelToRefill);
+            double expected = beforeRefuel + fuelToRefill;
+            double actual = car.FuelAmount;
 
-            double expectedAmount = beforeRefueling + fuelToRefuel;
-
-            double actualFuelAmount = car.FuelAmount;
-
-            Assert.AreEqual(expectedAmount, actualFuelAmount);
+            Assert.AreEqual(expected, actual);
         }
 
-        [TestCase(74)]
-        [TestCase(95)]
-        public void TEST_REFUEL_RETURNS_FUELCAPACITY_WHEN_EXCEEDED(double fuelToRefuel)
+        [TestCase(50)]
+        [TestCase(60)]
+        public void TEST_REFUEL_SHOULD_RETURN_FUELCAPACITY_IF_TANKEXCEEDED(double fuelToRefill)
         {
-            car = new Car("Audi", "A8", 3.4, 70);
-            car.Refuel(fuelToRefuel);
+            car = new Car("Ferrari", "F40", 25, 40);
+            car.Refuel(fuelToRefill);
 
-            double expected = car.FuelCapacity;
+            double expected = car.FuelAmount;
+            double actual = car.FuelCapacity;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TEST_DRIVE_METHOD_SHOULD_DECREASE_FUELAMOUNT()
+        {
+            ////double fuelNeeded = (distance / 100) * this.FuelConsumption;
+            //car = new Car("Ferrari", "F40", 23, 120);
+            //car.Refuel(50);
+
+            //car.Drive(30);
+
+            //double expected = 45.4;
+            //double actual = car.FuelAmount;
+
+            //Assert.AreEqual(expected, actual);
+            car = new Car("Ferrari", "F40", 6, 100);
+            car.Refuel(60);
+            car.Drive(40);
+
+            double expected = 57.6;
             double actual = car.FuelAmount;
 
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void TEST_DRIVE_SHOULD_DECREASE_CAR_FUELAMOUNT_IF_ENOUGH_FUEL()
-        {
-            //double fuelNeeded = (distance / 100) * this.FuelConsumption;
-            car = new Car("Audi", "A8", 3.4, 70);
-            //first refuel the car, by default has 0 fuel
-            car.Refuel(70);
-
-            car.Drive(45);
-
-            double expected = car.FuelAmount - ((45 / 100) * car.FuelConsumption);
-            double actual = car.FuelAmount;
-
-            Assert.AreEqual(expected, actual);
-        }
-        [Test]
-        public void TEST_DRIVE_SHOULD_THROW_EXCEPTION_IF_NOT_ENOUGH_FUEL()
+        public void TEST_DRIVE_METHOD_SHOULD_THROW_EXCEPTION_IF_NOT_ENOUGH_FUELAMOUNT()
         {
             Assert.Throws<InvalidOperationException>(() =>
             {
-                car.Drive(10);
+                car.Drive(20);
             }, "You don't have enough fuel to drive!");
         }
+
+        //[Test]
+        //public void TEST_FUELAMOUNT_SHOULD_THROW_EXCEPTION_IF_NEGATIVE()
+        //{
+        //    MethodInfo method = typeof(Car).GetMethod("FuelAmount", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        //    object[] parameters = { -2 };
+
+        //    Assert.Throws<ArgumentException>(() =>
+        //    {
+        //        object result = method.Invoke(car, parameters);
+        //    }, "Fuel amount cannot be negative!");
+        //}
     }
 }
--- C# OOP 22'/UnitTesting/UnitTesting - Exercise/ReadMe.md
+++ C# OOP 22'/UnitTesting/UnitTesting - Exercise/ReadMe.md
@@ -1,2 +1,5 @@
+# Update: CarManager Refactored 100/100
+-problem in Test_DriveMethodShouldDecreseFuelAMount solved
+
 ## CarManager 90/100
 ### Refactoring needed
