bfe6298ae92c7688d45c80958594a59408e41d2f
achkatam <agmatevv@gmail.com>
2022-12-18 08:56:18 -0500

    FileWriter inherits IWriter Interface

--- C# OOP 22'/Exam 10 Dec 2022/01. ChristmasPastry/IO/FileWriter.cs
+++ C# OOP 22'/Exam 10 Dec 2022/01. ChristmasPastry/IO/FileWriter.cs
@@ -1,8 +1,31 @@
 ﻿namespace ChristmasPastryShop.IO
 {
+    using System.IO;
     using ChristmasPastryShop.IO.Contracts;
 
     public class FileWriter : IWriter
     {
+        public FileWriter()
+        {
+            using (StreamWriter writer = new StreamWriter("../../../output.txt", false))
+            {
+                writer.Write("");
+            }
+        }
+        public void Write(string message)
+        {
+            using (StreamWriter writer = new StreamWriter("../../../output.txt", true))
+            {
+                writer.Write(message);
+            }
+        }
+
+        public void WriteLine(string message)
+        {
+            using (StreamWriter writer = new StreamWriter("../../../output.txt", true))
+            {
+                writer.WriteLine(message);
+            }
+        }
     }
 }
