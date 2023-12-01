e29d238b712fb3678efbc4a79929b4d8dc2da15b
achkatam <agmatevv@gmail.com>
2023-12-01 08:39:35 -0500

    added ex

--- Python/Fundamentals/Exam practice/exam+prep/4/ad_astra.py
+++ Python/Fundamentals/Exam practice/exam+prep/4/ad_astra.py
@@ -1,20 +1,20 @@
 import re
 
-input_text = input()
+text = input()
 
 pattern = r'#([\w+\s]+)#(\d{2}\/\d{2}\/\d{2})#(\d+)#|\|([\w+\s]+)\|(\d{2}\/\d{2}\/\d{2})\|(\d+)\|'
-filtered_products = re.findall(pattern, input_text)
+filtered = re.findall(pattern, text)
 clear_list = []
 
-for row in filtered_products:
+for row in filtered:
     cleared = []
     for value in row:
         if value:
             cleared.append(value)
     clear_list.append(cleared)
 
-total_calories = sum([int(calories[2]) for calories in clear_list])
-days = total_calories // 2000
+total_cals = sum(int(calories[2]) for calories in clear_list)
+days = total_cals // 2000
 print(f"You have food to last you for: {days} days!")
 
 if days > 0:
