1fc1ce4f25676119f28cea73ba119b2706cf6713
achkatam <agmatevv@gmail.com>
2023-12-01 09:23:46 -0500

    added ex

--- Python/Fundamentals/Exam practice/exam+prep/4/the_pianist.py
+++ Python/Fundamentals/Exam practice/exam+prep/4/the_pianist.py
@@ -1,62 +1,61 @@
 class Symphony:
-    def __init__(self, name, author, key):
-        self.name = name
-        self.author = author
+    def __init__(self, composer, key):
+        self.composer = composer
         self.key = key
 
 
-def change_key(pianist, symphony_name: str, new_key: str):
-    if symphony_name not in pianist.keys():
-        print(f"Invalid operation! {symphony_name} does not exist in the collection.")
-        return
+def solution(cmd_list: list, symphonies: dict):
+    for command in cmd_list:
+        tokens = command.split("|")
+        cmd = tokens[0]
 
-    pianist[symphony_name].key = new_key
-    print(f"Changed the key of {symphony_name} to {new_key}!")
+        if cmd == "Add":
+            symphony = tokens[1]
+            composer = tokens[2]
+            key = tokens[3]
+            if symphony in symphonies.keys():
+                print(f"{symphony} is already in the collection!")
+                continue
 
+            symphonies[symphony] = Symphony(composer, key)
+            print(f"{symphony} by {composer} in {key} added to the collection!")
+        elif cmd == "Remove":
+            symphony = tokens[1]
+            if symphony not in symphonies.keys():
+                print(f"Invalid operation! {symphony} does not exist in the collection.")
+                continue
 
-def remove(pianist, symphony_name: str):
-    if symphony_name not in pianist.keys():
-        print(f"Invalid operation! {symphony_name} does not exist in the collection.")
-        return
+            del (symphonies[symphony])
+            print(f"Successfully removed {symphony}!")
+        elif cmd == "ChangeKey":
+            symphony = tokens[1]
+            new_key = tokens[2]
+            if symphony not in symphonies.keys():
+                print(f"Invalid operation! {symphony} does not exist in the collection.")
+                continue
 
-    del pianist[symphony_name]
-    print(f"Successfully removed {symphony_name}!")
+            symphonies[symphony].key = new_key
+            print(f"Changed the key of {symphony} to {new_key}!")
+        elif cmd == "Stop":
+            for key, value in symphonies.items():
+                print(f"{key} -> Composer: {value.composer}, Key: {value.key}")
 
-
-def add(pianist, symphony_name: str, composer: str, key: str):
-    if symphony_name in pianist.keys():
-        print(f"{symphony_name} is already in the collection!")
-        return
-
-    pianist[symphony_name] = Symphony(symphony_name, composer, key)
-    print(f"{symphony_name} by {composer} in {key} added to the collection!")
-
-
-pianist = {}
 
 num = int(input())
+symphonies = {}
 
 for i in range(num):
-    input_data = input().split('|')
-    symphony_name, author, key = input_data[0], input_data[1], input_data[2]
-    pianist[symphony_name] = Symphony(symphony_name, author, key)
+    command = input().split('|')
+    symphony, composer, key = command[0], command[1], command[2]
+    symphonies[symphony] = Symphony(composer, key)
 
-command = input()
+cmd_list = []
 
-while command != "Stop":
-    tokens = command.split('|')
-    cmd, symphony_name = tokens[0], tokens[1]
-
-    if cmd == "Add":
-        composer = tokens[2]
-        key = tokens[3]
-        add(pianist, symphony_name, composer, key)
-    elif cmd == "Remove":
-        remove(pianist, symphony_name)
-    elif cmd == "ChangeKey":
-        new_key = tokens[2]
-        change_key(pianist, symphony_name, new_key)
+while True:
     command = input()
+    cmd_list.append(command)
 
-for symphony_name, symphony in pianist.items():
-    print(f"{symphony_name} -> Composer: {symphony.author}, Key: {symphony.key}")
+    if command == "Stop":
+        break
+
+solution(cmd_list, symphonies)
