bcb4fcdd35c2620227cea61735bbdad0d135c33f
achkatam <agmatevv@gmail.com>
2023-12-02 12:52:24 -0500

    refactored model's phone from int to charField

--- Python/Fundamentals/Basic Web Project/djangoproject/djangoproject/phonebook/models.py
+++ Python/Fundamentals/Basic Web Project/djangoproject/djangoproject/phonebook/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=30)
-    number = models.IntegerField()
+    number = models.CharField(max_length=14)
