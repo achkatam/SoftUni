655349a6b80219ed0a5cf8b9558979c864699dfc
achkatam <agmatevv@gmail.com>
2023-12-02 12:57:29 -0500

    added __str__ to represent the Contacts by their names

--- Python/Fundamentals/Basic Web Project/djangoproject/djangoproject/phonebook/models.py
+++ Python/Fundamentals/Basic Web Project/djangoproject/djangoproject/phonebook/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=30)
     number = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.name
