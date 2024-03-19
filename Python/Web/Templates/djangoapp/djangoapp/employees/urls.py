da1fc8269bd7c369e87d0e227b5675861b6bcdca
Angel Matev <100487082+achkatam@users.noreply.github.com>
2024-03-19 13:17:13 -0400

    Delete Python/Web/Templates directory

--- Python/Web/Templates/djangoapp/djangoapp/employees/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from djangoapp.employees import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("employees/<int:pk>", views.employee_details, name="employee_details")
-]
