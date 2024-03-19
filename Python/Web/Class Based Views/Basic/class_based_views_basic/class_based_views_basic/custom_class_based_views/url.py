22e7c6cbd23a7a2a4ac915a387b74aed7af2b5a5
Angel Matev <100487082+achkatam@users.noreply.github.com>
2024-03-19 13:15:17 -0400

    Delete Python/Web/Class Based Views directory

--- Python/Web/Class Based Views/Basic/class_based_views_basic/class_based_views_basic/custom_class_based_views/url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from class_based_views_basic.custom_class_based_views.views import index, IndexView
-
-urlpatterns = (
-    path("", index, name="ccvc_index"),
-    path("cbv/", IndexView.as_view(), name="cbv_index"),
-)
--- Python/Web/Class Based Views/Basic/class_based_views_basic/class_based_views_basic/web/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'class_based_views_basic.web'
