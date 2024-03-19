afb2e2a210a985700af6417c5b23ff93451c1ee6
Angel Matev <100487082+achkatam@users.noreply.github.com>
2024-03-19 13:16:44 -0400

    Delete Python/Web/FormsAdvanced directory

--- Python/Web/FormsAdvanced/forms_advanced/forms_advanced/web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from forms_advanced.web.views import index, create_person, show_formset
-
-urlpatterns = (
-    path('', index, name="index"),
-    path("person/create", create_person, name="create_person"),
-    path("formsets/", show_formset, name="show_formset"),
-)
